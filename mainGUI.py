8fba45869b1996ac05059b411c6d3cf50eb47c74
Leonid-Dalin <petreDalin@gmail.com>
2022-02-07 02:53:34 +0200

    add trueskill

--- mainGUI.py
+++ mainGUI.py
@@ -1,10 +1,10 @@
+from textwrap import wrap
 import PySimpleGUI as sg
 import os
 
 from ctypes import alignment
 from tkinter import *
-from tkinter import tkk
-
+from trueskill import Rating
 
 
 root = Tk()
@@ -14,35 +14,53 @@ root.title("CSGOForecaster")
 root.geometry("640x480")
 
 
+
 def showCredits(event):
     creditsWindow = Toplevel(root)
     creditsWindow.geometry("640x480")
     creditsWindow.title("CSGOForecaster > Credits")
 
     main_frame = Frame(creditsWindow)
-    main_frame.pack(fill=BOTH, expand=1)
+    main_frame.pack(fill=BOTH, expand=1, pady=20, padx=20)
 
+   
     my_canvas = Canvas(main_frame)
-    my_canvas.pack(side=LEFT, fill=BOTH, expand=1)
+    my_canvas.pack(side=LEFT, fill=BOTH, )
 
     hScrollbar = Scrollbar(main_frame, orient=VERTICAL, command=my_canvas.yview)
-    
-    wScrollbar = Scrollbar(main_frame, orient=HORIZONTAL, command=my_canvas.xview)
     hScrollbar.pack(side=RIGHT, fill=Y)
-    wScrollbar.pack(side=TOP, fill=X)
+    wScrollbar = Scrollbar(main_frame, orient=HORIZONTAL, command=my_canvas.xview)
+    wScrollbar.pack(side=BOTTOM, fill=X)
 
-    my_canvas.configure(yscrollcommand=hScrollbar.set, xscrollcommand=wScrollbar.set)
-    my_canvas.bind('<Configure>', lambda e: my_canvas.configure(scrollregion=my_canvas.bbox("all")))
-
+    
+  
+    """"
     second_frame = Frame(my_canvas)
+    
 
     my_canvas.create_window((0,0), window=second_frame, anchor="nw")
+    
+    # Create our Scrollbar
+    text_scroll = Scrollbar(main_frame)
+    text_scroll.pack(side=RIGHT, fill=Y)
 
-    f = open('Documents/_java/CSGOForecaster/credits.txt', 'r', encoding='utf-8')
+    # Create Two Text Boxes
+    my_text1 = Text(main_frame, width=20, height=25, font=("Helvetica", 16), yscrollcommand=text_scroll.set, wrap="none")
+    my_text1.pack(side=RIGHT, padx=5)
+    my_text2 = Text(main_frame, width=20, height=25, font=("Helvetica", 16), yscrollcommand=text_scroll.set, wrap="none")
+    my_text2.pack(side=LEFT)
+
+    # Configure Scrollbar
+    text_scroll.config(command=multiple_yview)
+     """
+    
+    f = open('credits.html', 'r', encoding='utf-8')
     file_contents = f.read()
-    creditsLabel = Label(second_frame, text=file_contents, justify=LEFT)
+    creditsLabel = Text(my_canvas, text=file_contents, justify=LEFT)
     f.close()
-    creditsLabel.pack()
+    my_canvas.configure(yscrollcommand=hScrollbar.set, xscrollcommand=wScrollbar.set)
+    my_canvas.bind('<Configure>', lambda e: my_canvas.configure(scrollregion=my_canvas.bbox("all")))
+    creditsLabel.pack(side=TOP, fill=X)
     
     
 def createNewWindow():
